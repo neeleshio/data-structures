cac5e9288da220b9b1d6a1075ec317307e493bbc
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-16 18:28:40 +0530

    get, set, insert, remove, reverse methods added

--- linkedList.js
+++ linkedList.js
@@ -76,6 +76,75 @@ class LinkedList {
 
         return temp
     }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined
+
+        let temp = this.head
+        for (let i = 0; i < index; i++) {
+            temp = temp.next
+        }
+
+        return temp
+    }
+
+    set(index, value) {
+        let temp = this.get(index)
+
+        if (temp) {
+            temp.value = value
+            return true
+        }
+        return false
+    }
+
+    insert(index, value) {
+        if (index < 0 || index > this.length) return undefined
+        if (index === 0) return this.unshift(value)
+        if (index === this.length) return this.push(value)
+
+        const newNode = new Node(value)
+        let temp = this.get(index - 1)
+
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+
+        return this
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+
+        let before = get(index)
+        let temp = before.next
+
+        before.next = temp.next
+        temp.next = null
+        this.length--
+
+        return this
+    }
+
+    reverse() {
+        let temp = this.head
+        this.head = this.tail
+        this.tail = temp
+
+        let prev = null
+        let next = temp.next
+
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+        }
+
+        return this;
+    }
 }
 
 let myLinkedList = new LinkedList(1)
