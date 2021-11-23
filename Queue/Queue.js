74d81bca683b820952c33a9638f6924659b142c9
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-23 11:40:07 +0530

    enqueue & dequeue

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -12,4 +12,32 @@ class Queue {
         this.last = newNode;
         this.length = 1
     }
+
+    enqueue(value) {
+        const newNode = new Node(value)
+
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        this.length++
+        return this
+    }
+
+    dequeue() {
+        if (this.length === 0) return undefined
+        let temp = this.first
+        if (this.length === 1) {
+            this.first = null
+            this.last = null
+        } else {
+            this.first = the.first.next
+            temp.next = null
+        }
+        this.length--
+        return temp
+    }
 }
