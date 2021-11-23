71daa5f980847506948b04220d4cfd1df4f95cc3
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-23 15:02:24 +0530

    bst contains

--- BST/BST.js
+++ BST/BST.js
@@ -36,6 +36,22 @@ class BST {
             }
         }
     }
+
+    contains(value) {
+        if (this.root === null) return false
+        let temp = this.root
+
+        while (temp) {
+            if (value < temp.value) {
+                temp = temp.left
+            } else if (value > temp.value) {
+                temp = temp.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
 }
 
 const myBST = new BST()
